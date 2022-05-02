edb95ffbbf3414681edf623351b49b093e4b6743
Penn Song <pennsong07@gmail.com>
2022-05-02 14:35:57 +0800

    improved: 添加redirect和openApi相关的处理

--- src/main/java/com/ugeez/exception/config/GlobalResponseAdvice.java
+++ src/main/java/com/ugeez/exception/config/GlobalResponseAdvice.java
@@ -20,7 +20,17 @@ import java.util.LinkedHashMap;
 public class GlobalResponseAdvice implements ResponseBodyAdvice {
     @Override
     public boolean supports(MethodParameter returnType, Class converterType) {
+        String returnTypeString = returnType.toString();
+        if (
+                returnTypeString.contains("redirectToUi")
+                        ||
+                        returnTypeString.contains("openapi")
+        ) {
+            // redirect和openApi相关
+            return false;
+        }
         return true;
+
     }
 
     @Override
@@ -34,6 +44,20 @@ public class GlobalResponseAdvice implements ResponseBodyAdvice {
         if (selectedContentType.equalsTypeAndSubtype(
                 MediaType.APPLICATION_JSON
         )) {
+            if (body instanceof LinkedHashMap) {
+                LinkedHashMap map = (LinkedHashMap) body;
+                // 处理GlobalError如404
+                if (map.get("status") != null) {
+                    int status = (int) map.get("status");
+                    response.setStatusCode(HttpStatus.valueOf(status));
+                    UGJsonResponse result = UGJsonResponse.globalError(status, map.get("message").toString());
+                    return result;
+                } else {
+                    // TODO: log 目前还没考虑到的globalError
+                    return UGJsonResponse.error(new UGException(UGExceptionType.OTHER_ERROR));
+                }
+            }
+
             if (body instanceof UGJsonResponse) {
                 // 有出错的情况
                 response.setStatusCode(HttpStatus.valueOf(
@@ -41,25 +65,13 @@ public class GlobalResponseAdvice implements ResponseBodyAdvice {
                 ));
                 return body;
             } else {
-                if (body instanceof LinkedHashMap) {
-                    // globalError 如 404
-                    LinkedHashMap map = (LinkedHashMap) body;
-                    if (map.get("status") != null) {
-                        int status = (int) map.get("status");
-                        response.setStatusCode(HttpStatus.valueOf(status));
-                        UGJsonResponse result = UGJsonResponse.globalError(status, map.get("message").toString());
-                        return result;
-                    } else {
-                        // TODO: log 目前还没考虑到的globalError
-                        return UGJsonResponse.error(new UGException(UGExceptionType.OTHER_ERROR));
-                    }
-                }
                 // 正常返回数据的情况
                 response.setStatusCode(HttpStatus.OK);
                 UGJsonResponse result = UGJsonResponse.success(body);
                 return result;
             }
         }
+        // TODO: log 不应该发生
         log.info("非json情况:" + body);
         return body;
     }
